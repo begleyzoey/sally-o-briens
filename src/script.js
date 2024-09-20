9dd1f2084a525d95aa64d8009098605fc30edb7e
begleyzoey <begleyzoey@gmail.com>
2024-09-20 16:50:55 +0100

    created basic landing page

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function scrollPage() {
+  const my_element = document.getElementById("element");
+
+  element.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+}
+async;
+src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyBqCdzKBQybTG_GA7FFwXs1103Pqe1y9xg&callback=console.debug&libraries=maps,marker&v=beta";
